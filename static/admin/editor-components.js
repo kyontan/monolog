5af9be150bdc3abfe5355e42e1ab6e5d089909aa
kyontan <kyontan@monora.me>
2026-09-09 00:35:32 -0500

    Decap editor components: read-more + horizontal rule

--- static/admin/editor-components.js
+++ static/admin/editor-components.js
@@ -1,5 +1,6 @@
-/* Decap editor component: one-click <!--more--> (read more split) insert.
- * Appears in the markdown editor toolbar. Preview shows a divider label.
+/* Decap editor components: one-click inserts for theme idioms.
+ * - more: <!--more--> read-more split (list truncation + jump anchor)
+ * - hr: --- horizontal rule
  */
 (function () {
   function register() {
@@ -17,6 +18,21 @@
         },
         toPreview: function () {
           return "<hr><p>続きを読む：ここから下は一覧に出ません</p><hr>";
+        },
+      });
+      window.CMS.registerEditorComponent({
+        id: "hr",
+        label: "水平線",
+        fields: [],
+        pattern: /^---$/m,
+        fromBlock: function () {
+          return {};
+        },
+        toBlock: function () {
+          return "---";
+        },
+        toPreview: function () {
+          return "<hr>";
         },
       });
     } else {
